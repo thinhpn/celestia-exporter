beee469ca9d4d2034404055676e57d7abd4e5280
thinhpn <ngocthinhbk@gmail.com>
2023-05-06 22:36:47 +0700

    add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps: [
+        {
+            name: "Celestia Exporter App",
+            script: "/usr/bin/yarn",
+            args: "app",
+            instances: 1,
+            exec_mode: "cluster",
+            autorestart: true,
+            max_memory_restart: "500M",
+        },        
+    ],
+};
